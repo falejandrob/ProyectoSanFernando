cd1b1b4d15c6a8e84de0b1214c62f8f1753f3a0c
PriceST1 <falejandro.borjas@gmail.com>
2023-03-24 20:06:57 +0100

    Creados los modelos

---
+++ database/seeders/LineaPedidoSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LineaPedidoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
