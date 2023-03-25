115d012ef445dcd6fb5c30a1f06ccfde0639e7d0
PriceST1 <falejandro.borjas@gmail.com>
2023-03-25 17:30:01 +0100

    modificado el .env.example

--- config/app.php
+++ config/app.php
@@ -195,6 +195,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * provider from spatie
+         * */
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
