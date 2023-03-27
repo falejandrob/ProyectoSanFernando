207bdd389e54b08c04097c2221f37f5445bee515
n03fs <nahiafs@iescastelar.com>
2023-03-27 19:19:24 +0200

    Cambios en pantalla principal - Estructura inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,6 +30,9 @@
                 </button>
             </div>
         </div>
+        <div style="width: 100%; text-align: center; padding: 10px">
+            <button style="font-size: 130%; width: 50%; padding: 10px" type="button" class="btn btn-success">Hacer pedido</button>
+        </div>
     </div>
 
     <div class="productos" style="width: 70%; padding: 15px">
@@ -45,7 +48,11 @@
                 <div style="text-align: center; padding: 10px">
                     <h5 style="margin: 15px">Kilo de patatas</h5>
                     <input style="width: 50%; margin: auto; text-align: center" min="1" value="1" type="number" class="form-control">
-                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">Pedir</button>
+                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
             <div style="width: 20%; height: 350px; background: #9ca3af; padding: 15px; margin: 5px">
@@ -55,7 +62,11 @@
                 <div style="text-align: center; padding: 10px">
                     <h5 style="margin: 15px">Kilo de harina</h5>
                     <input style="width: 50%; margin: auto; text-align: center" min="1" value="1" type="number" class="form-control">
-                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">Pedir</button>
+                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
             <div style="width: 20%; height: 350px; background: #9ca3af; padding: 15px; margin: 5px">
@@ -65,7 +76,11 @@
                 <div style="text-align: center; padding: 10px">
                     <h5 style="margin: 15px">Kilo de harina</h5>
                     <input style="width: 50%; margin: auto; text-align: center" min="1" value="1" type="number" class="form-control">
-                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">Pedir</button>
+                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
             <div style="width: 20%; height: 350px; background: #9ca3af; padding: 15px; margin: 5px">
@@ -75,7 +90,11 @@
                 <div style="text-align: center; padding: 10px">
                     <h5 style="margin: 15px">Kilo de harina</h5>
                     <input style="width: 50%; margin: auto; text-align: center" min="1" value="1" type="number" class="form-control">
-                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">Pedir</button>
+                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
             <div style="width: 20%; height: 350px; background: #9ca3af; padding: 15px; margin: 5px">
@@ -85,7 +104,11 @@
                 <div style="text-align: center; padding: 10px">
                     <h5 style="margin: 15px">Kilo de harina</h5>
                     <input style="width: 50%; margin: auto; text-align: center" min="1" value="1" type="number" class="form-control">
-                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">Pedir</button>
+                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
             <div style="width: 20%; height: 350px; background: #9ca3af; padding: 15px; margin: 5px">
@@ -95,7 +118,11 @@
                 <div style="text-align: center; padding: 10px">
                     <h5 style="margin: 15px">Kilo de harina</h5>
                     <input style="width: 50%; margin: auto; text-align: center" min="1" value="1" type="number" class="form-control">
-                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">Pedir</button>
+                    <button style="width: 75%; margin: 15px; font-size: 120%" type="button" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
