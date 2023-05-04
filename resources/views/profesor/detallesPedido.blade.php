9889efd52a46a29d1fc817d051007108c7f09059
PriceST1 <falejandro.borjas@gmail.com>
2023-05-04 19:50:17 +0200

    Añadida la persistencia del carrito

--- resources/views/profesor/detallesPedido.blade.php
+++ resources/views/profesor/detallesPedido.blade.php
@@ -2,8 +2,10 @@
 @section('content')
     <div class="container" style="padding: 25px">
         <a class="btn btn-secondary" href="{{ route('misPedidos', Auth::user()->id) }}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                 class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                      d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
             </svg>
             Volver
         </a>
@@ -21,34 +23,32 @@
 
         <br><br>
 
-        <h2>Lineas pedido</h2>
+        <h2>Productos</h2>
         <hr>
 
         @if(count($pedido) == 0)
-            <h3>El pedido no tiene ninguna línea</h3>
+            <h3>El pedido no tiene ningun producto</h3>
         @else
-            <table class="table" style="text-align: center; font-size: 120%">
-                <thead>
-                <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">Id producto</th>
-                    <th scope="col">Nombre producto</th>
-                    <th scope="col">Categoria producto</th>
-                    <th scope="col">Cantidad</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($pedido as $linea)
+            <div class="table-responsive table-wrapper-scroll-y my-custom-scrollbar">
+                <table class="table mb-0 tabla-scroll" style="text-align: center; font-size: 120%">
+                    <thead>
                     <tr>
-                        <th scope="row">{{ $linea->rowId }}</th>
-                        <td>{{ $linea->id }}</td>
-                        <td>{{ $linea->name }}</td>
-                        <td>{{ $linea->options->categoria }}</td>
-                        <td>{{ $linea->qty }}</td>
+                        <th scope="col">Nombre producto</th>
+                        <th scope="col">Categoria producto</th>
+                        <th scope="col">Cantidad</th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    @foreach($pedido as $linea)
+                        <tr>
+                            <td>{{ $linea->name }}</td>
+                            <td>{{ $linea->options->categoria }}</td>
+                            <td>{{ $linea->qty }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         @endif
     </div>
 @endsection
